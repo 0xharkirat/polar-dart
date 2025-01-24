0475c3f6c0e20e9af88d55d185939d393ab2f3ed
Harkirat Singh <sandhukirat23@gmail.com>
2025-01-24 21:19:06 +1100

    add: dotenv files.

--- example/polar_dart_example.dart
+++ example/polar_dart_example.dart
@@ -1,16 +1,21 @@
 import 'package:polar_dart/polar_dart.dart';
 import 'package:polar_dart/src/polar_environment.dart';
+import 'package:dotenv/dotenv.dart';
 
 void main() async {
   // Initialize the client for the sandbox environment
+
+  // Load the .env file
+  var env = DotEnv()..load();
+
   final polarClient = PolarClient(
-    apiKey: 'your_api_key_here',
+    apiKey: env['POLAR_API_KEY']!, // Use the API key from the .env file
     environment: PolarEnvironment.sandbox, // Use sandbox environment
   );
 
   try {
     // Example: Fetching data from a test endpoint
-    final response = await polarClient.get('/v1/products');
+    final response = await polarClient.get('/v1/organizations/');
     print('Response: $response');
   } catch (e) {
     print('Error: $e');
